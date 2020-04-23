2248adb9220ffe2f9ee9484da1e08aea4b1bbd67
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-04-23 16:46:24 -0300

    fixing formatDate function

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,36 +1,7 @@
 const formatDate = (date: Date): string => {
-  const parsedDate = new Date(date);
+  const parsedDate = new Date(date).toLocaleDateString('pt-BR');
 
-  const day = parsedDate.getUTCDate();
-  const month = parsedDate.getUTCMonth();
-  const year = parsedDate.getUTCFullYear();
-
-  let testD = 0;
-  let testM = 0;
-
-  if (day < 10) {
-    testD = 1;
-  }
-
-  if (month < 10) {
-    testM = 1;
-  }
-
-  let finalDate = '';
-
-  if (testD !== 0 && testM !== 0) {
-    finalDate = `0${day}/0${month}/${year}`;
-  }
-  if (testD !== 0 && testM === 0) {
-    finalDate = `0${day}/${month}/${year}`;
-  }
-  if (testD === 0 && testM !== 0) {
-    finalDate = `${day}/0${month}/${year}`;
-  } else {
-    finalDate = `${day}/${month}/${year}`;
-  }
-
-  return finalDate;
+  return parsedDate;
 };
 
 export default formatDate;
